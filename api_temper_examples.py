155ed2afd409d4f744f76185bc93cfc42a816cb5
JuliaGofman <35265346+JuliaGofman@users.noreply.github.com>
2018-07-21 15:43:08 +0500

    API_trying

---
+++ api_temper_examples.py
@@ -0,0 +1,26 @@
+''' API-key ee49aeee22cae34422a9770aa994fa77
+
+'''
+import requests
+
+api_url = "http://api.openweathermap.org/data/2.5/weather"
+city = input("Input city: ")
+
+params = {
+    'q': city,
+    'appid': 'ee49aeee22cae34422a9770aa994fa77',
+    'units': 'metric'
+}
+
+res = requests.get(api_url, params=params)
+# print(res.status_code)
+# print(res.headers['Content-Type'])
+# print(res.json())  or equal return json.loads(res.text)
+
+data = res.json()
+template = "Current temperature in {} is {}\u2103"
+pressure = "Current pressure is {} hPa"
+humidity = "Current humidity is {}\u0025"
+print(template.format(city, data["main"]["temp"]))
+print(pressure.format(data["main"]["pressure"]))
+print(humidity.format(data["main"]["humidity"]))
